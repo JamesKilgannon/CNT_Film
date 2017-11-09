32c213e96d4380ec52842e49f1b0361ed5389b9a
James Kilgannon <kilgannon.j@husky.neu.edu>
2017-11-09 13:48:14 -0500

    Added code for checking if CNTs intersect.

--- CNT Film.py
+++ CNT Film.py
@@ -1,49 +1,61 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 import numpy as np
 
 
-# In[ ]:
+# In[105]:
 
 #Important variables
 network_size = 1000 #side length of network boundaries
+CNT_length_normal = 1000 #normal length of CNT at center of distribution
+CNT_length_stddev = 2 #standard deviation of CNT length from normal
+CNT_num_tubes = 100 #number of tubes in film
+
+CNT_init = np.zeros((CNT_num_tubes,6))
+
+#Generating tube information
+#randomly assigning tube lengths distributed around a set tube length
+CNT_init[:,0] = np.random.normal(CNT_length_normal, CNT_length_stddev, CNT_num_tubes)
+
+#randomly assign starting point and orientation
+CNT_init[:,1:4] = np.random.rand(CNT_num_tubes, 3)
+
+#applying scaling to random numbers so they match the needed values
+scaling_factor = np.array([1, network_size, network_size, 2*np.pi, 1, 1])
+CNT_init = CNT_init * scaling_factor
+
+#calculating the x-range for the tubes
+CNT_init[:,5] = np.cos(CNT_init[:,3]) * CNT_init[:,0]
+
+#calculating slope
+CNT_init[:,3] = np.tan(CNT_init[:,3])
+
+#calculating the y-intercept of the lines
+CNT_init[:,4] = CNT_init[:,2] - CNT_init[:,3] * CNT_init[:,2]
+
+header = ['Length','x-value','y-value','slope','y-intercept','x-high']
+print(header)
+#print(CNT_init)
+
+
+# In[106]:
+
+#array_size = (CNT_init[:,0].size,CNT_init[0,:].size)
+CNT_intersect = np.zeros((CNT_num_tubes,CNT_num_tubes),dtype=bool)
+for i in range(0,CNT_num_tubes):
+    m1 = CNT_init[i,3]
+    b1 = CNT_init[i,4]
+    for j in range(i+1,CNT_num_tubes):
+        x_intersect = (CNT_init[j,4] - b1) / (m1 - CNT_init[j,3])
+        if CNT_init[i,1] <= x_intersect <= CNT_init[i,5] and CNT_init[j,1] <= x_intersect <= CNT_init[j,5]:
+            CNT_intersect[i,j] = True
+print(CNT_intersect)
 
 
 # In[ ]:
-
-def CNT():
-    #randomly assign length distributed around a set tube length
-        #need to average tube length and distribution
-    #randomly assign starting point
-    #randomly assign angle between 0 and 180
-    
-
-
-# In[2]:
-
-def EOL(endpoint_1,endpoint_2):
-    #Determines the slope, y-intercept of the parent line, and range of x-values of a line
-    #segment made by 2 points. Endpoints are input as a list of x and y values e.g. [x,y]
-    x1 = endpoint_1[0]
-    y1 = endpoint_1[1]
-    x2 = endpoint_2[0]
-    y2 = endpoint_2[1]
-    
-    #Preventing undefined values for infinite slope
-    if x1 == x2:
-        slope = 1e10
-    else:
-        slope = (y2 - y1) / (x2 - x1)
-    y_intercept = y1 - slope * x1
-    #finding range of x-values the segment runs for to make determining intersection easier
-    x_range = [np.min(x1,x2),np.max(x1,x2)]
-    return slope, y_intercept, x_range
-
-
-# In[3]:
 
 def intersect(EOL_1,EOL_2):
     #Calculates if two line segments intersect given two equations of lines given the
@@ -67,10 +79,31 @@ def intersect(EOL_1,EOL_2):
 
 # In[ ]:
 
-def y_delta
+def EOL(endpoint_1,endpoint_2):
+    #Determines the slope, y-intercept of the parent line, and range of x-values of a line
+    #segment made by 2 points. Endpoints are input as a list of x and y values e.g. [x,y]
+    x1 = endpoint_1[0]
+    y1 = endpoint_1[1]
+    x2 = endpoint_2[0]
+    y2 = endpoint_2[1]
+    
+    #Preventing undefined values for infinite slope
+    if x1 == x2:
+        slope = 1e10
+    else:
+        slope = (y2 - y1) / (x2 - x1)
+    y_intercept = y1 - slope * x1
+    #finding range of x-values the segment runs for to make determining intersection easier
+    x_range = [np.min(x1,x2),np.max(x1,x2)]
+    return slope, y_intercept, x_range
 
 
-# In[29]:
+# In[ ]:
+
+CNT_data = CNT_info(CNT_length_normal, CNT_length_stddev, CNT_num_tubes)
+
+
+# In[ ]:
 
 1e6
 
